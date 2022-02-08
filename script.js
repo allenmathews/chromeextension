0647f4fa71eee93002b192ea4f8f6c8c139a43e8
Allen Mathews <allenjoemathews@gmail.com>
2022-02-09 10:06:26 +1100

    added ul list

--- script.js
+++ script.js
@@ -3,11 +3,18 @@
 // }
 
 // clickbutton()
-let myLeads = [];
+let myLeads = ["a", "b", "c"];
+let ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
