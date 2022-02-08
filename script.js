cd6db247da4a11512985317e4514ed3a1d44209b
Allen Mathews <allenjoemathews@gmail.com>
2022-02-09 07:57:35 +1100

    event listener for the button added

---
+++ script.js
@@ -0,0 +1,11 @@
+// function saveLead() {
+//     console.log("Button clicked!")
+// }
+
+// clickbutton()
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
