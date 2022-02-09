864d61026fdba508415757906004b88f369ac2cd
Allen Mathews <allenjoemathews@gmail.com>
2022-02-09 13:52:37 +1100

    added dollar sign and string template literals

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // }
 
 // clickbutton()
-let myLeads = ["a", "b", "c"];
+let myLeads = [];
 let ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 
@@ -12,15 +12,25 @@ const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // const li = document.createElement("li") //create element
-    // li.textContent = myLeads[i] // set textContent to element
-    // ulEl.append(li) // appending to ul
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
 
-ulEl.innerHTML = listItems;
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        // const li = document.createElement("li") //create element
+        // li.textContent = myLeads[i] // set textContent to element
+        // ulEl.append(li) // appending to ul
+        listItems += `
+        <li> 
+        <a target="_blank" href="${myLeads[i]}">
+            ${myLeads[i]}  
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems;
+}
