2a91fb1ed0b218f4cdd4640eef105a83da1cbcfa
Allen Mathews <allenjoemathews@gmail.com>
2022-02-11 13:15:00 +1100

    refactored code involving render and passed in leads as argument

--- script.js
+++ script.js
@@ -7,44 +7,53 @@
 // myLeads.push("www.epicleads.com")
 // console.log(myLeads)
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn")
     // localStorage.setItem("myLeads", "www.examplelead.com")
     // console.log(localStorage.getItem("myLeads"))
     // localStorage.setItem("lead", "https://microsoft.com")
     // console.log(localStorage.getItem("lead"))
     // localStorage.clear()
 
+console.log(leadsFromLocalStorage)
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-    inputEl.value = ""
-    renderLeads()
-})
-
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
         // const li = document.createElement("li") //create element
         // li.textContent = myLeads[i] // set textContent to element
         // ulEl.append(li) // appending to ul
         listItems += `
         <li> 
-        <a target="_blank" href="${myLeads[i]}">
-            ${myLeads[i]}  
+        <a target="_blank" href="${leads[i]}">
+            ${leads[i]}  
             </a>
         </li>
         `
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function(e) {
+    console.log("doubleClicked!")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    inputEl.value = ""
+    render(myLeads)
+})
